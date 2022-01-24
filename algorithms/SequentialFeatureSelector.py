068b65e9e5143d7770236b7c6ecc0d742d811475
Manel Pereira <afonso.pereira4525@gmail.com>
2022-01-24 00:33:05 +0000

    Added a default estimator (GradientBoostingRegressor) to SequentialFeatureSelection

--- algorithms/SequentialFeatureSelector.py
+++ algorithms/SequentialFeatureSelector.py
@@ -1,11 +1,11 @@
 from numpy import number
 from IFeatureSelection import IFeatureSelection
 from sklearn.feature_selection import SelectKBest,f_regression, SequentialFeatureSelector
-
+from sklearn.ensemble import GradientBoostingRegressor
 class SequentialFeatureSelector(IFeatureSelection):
 
-    def __init__(self,number_of_features,*args):
-        self.model = SequentialFeatureSelector(_features_to_select=number_of_features,*args)
+    def __init__(self,*args,number_of_features=5,estimator=GradientBoostingRegressor()):
+        self.model = SequentialFeatureSelector(estimator=estimator,n_features_to_select=number_of_features,*args)
         self.number_of_features = number_of_features
 
     def fit(self,*kwargs):
