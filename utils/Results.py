506ebd7804d3efbae7f5762afb5c520387ab0c60
Manel Pereira <afonso.pereira4525@gmail.com>
2022-02-06 00:26:52 +0000

    Added Results class

---
+++ utils/Results.py
@@ -0,0 +1,31 @@
+
+class Results:
+
+    def __init__(self,x_test,y_test,x_train,y_train,model,features):
+        self.x_test = x_test
+        self.y_test = y_test
+        self.x_train = x_train
+        self.y_train = y_train
+        self.model = model
+        self.features = features
+    
+    def _get_x_test(self):
+        return self.x_test
+    def _get_y_test(self):
+        return self.y_test
+    def _get_x_train(self):
+        return self.x_train
+    def _get_y_train(self):
+        return self.y_train
+    def _get_model(self):
+        return self.model
+    def _get_features(self):
+        return self.features
+        
+    x_test = property(_get_x_test,None,None,"Gets the x_test")
+    y_test = property(_get_y_test,None,None,"Gets the y_test")
+    x_train = property(_get_x_train,None,None,"Gets the x_train")
+    y_train = property(_get_y_train,None,None,"Gets the y_train")
+    model = property(_get_model,None,None,"Gets the model")
+    features = property(_get_features,None,None,"Gets the features")
+    
