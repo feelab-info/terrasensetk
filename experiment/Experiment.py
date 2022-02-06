b636dec309e6e7f891857b00db5d9a04e49c6734
Manel Pereira <afonso.pereira4525@gmail.com>
2022-02-06 00:29:05 +0000

    Implemented execute method to Experiment class, missing the metrics part

--- experiment/Experiment.py
+++ experiment/Experiment.py
@@ -1,9 +1,11 @@
-from algorithms.IAlgorithm import IAlgorithm
-from performance.IFeatureSelection import IFeatureSelection
-from algorithms.ICrossValidation import ICrossValidation
-from dataset.Dataset import Dataset
-from parser.Parser import Parser
 
+from utils.Results import Results
+from ..algorithms.IAlgorithm import IAlgorithm
+from ..performance.IFeatureSelection import IFeatureSelection
+from ..algorithms.ICrossValidation import ICrossValidation
+from ..dataset.Dataset import Dataset
+from ..parser.Parser import Parser
+from sklearn.model_selection import train_test_split
 class Experiment:
     def __init__(self, dataset_parser,model, feature_selection=None, cross_validation=None,fit_for_variable="N"):
         if(not issubclass(Parser,dataset_parser)): raise TypeError("Not a subtype of Parser")
@@ -15,27 +17,40 @@ class Experiment:
         self.feature_selection = feature_selection
         self.cross_validation = cross_validation
         self.model = model
-        self.dataset_parser.create_dataframe(self.dataset_parser.dataset)
+        self.eopatch_ids = self.dataset_parser.create_dataframe(self.dataset_parser.dataset)["EOPATCH"]
+        self.eopatch_ids = self.eopatch_ids.unique()
         self.dataset_array = self.dataset_parser.create_array(fit_for_variable);
+        self.fit_for_variable = fit_for_variable
 
 
-    def execute(self):
+    def execute(self) -> list(Results):
         x,y = self.dataset_array
-        
+        features=None
         if(self.feature_selection is not None):
             x = self.feature_selection.fit(self.dataset_array)
-
+            features = self.feature_selection.get_model().get_feature_names_out(self.dataset_parser.features)
+        results = []
         if(self.cross_validation is not None):
-            folds = self.cross_validation.split()
+            folds = self.cross_validation.split(self.eopatch_ids)
+
+            for i,(train,test) in folds:
+                model = self.model.clone(self.model)
+                x_train,y_train = self.dataset_parser.create_array(self.fit_for_variable,image_ids=train,features=features)
+                x_test,y_test = self.dataset_parser.create_array(self.fit_for_variable,image_ids=test,features=features)
+                model.fit(x_train,y_train)
+                model.predict(x_test)
+                results.append(Results(x_train,y_test,x_train,y_train,model))
+        else:
+            train,test = train_test_split(self.eopatch_ids)
+            model = self.model.clone(self.model)
+            x_train,y_train = self.dataset_parser.create_array(self.fit_for_variable,image_ids=train,features=features)
+            x_test,y_test = self.dataset_parser.create_array(self.fit_for_variable,image_ids=test,features=features)
+            model.fit(x_train,y_train)
+            model.predict(x_test)
+            results.append(Results(x_train,y_test,x_train,y_train,model))
+        return results       
 
 
-
-        #TODO: prepare the data check
-        #TODO: feed the featureSelector check
-        #perform kfold if exists
-        #create Experiment object with information regarding the folds values and everything that we got
-        #do metrics evaluation
-        self.dataset_parser
 
     
     #impl
