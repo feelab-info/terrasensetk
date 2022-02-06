622a0762ae9da0da1d86efe41bf8e2b4c03ebb51
Manel Pereira <afonso.pereira4525@gmail.com>
2022-02-06 00:27:51 +0000

    Added features property to parser and new  arguments to create_array

--- parser/Parser.py
+++ parser/Parser.py
@@ -10,14 +10,18 @@ class Parser:
         if( not isinstance(dataset,Dataset)):
             
             raise TypeError("Must be a subtype of terrasensetk.Dataset")
+        self.indices = INDICES
+        self.bands = BANDS
         self.subset = subset
         self.dataset = dataset
     
-    def get_dataset(self):
-        return self.dataset
-
-    dataset = property(get_dataset,None,None,"""The dataset that was parsed""")
-
+    def _get_dataset(self):
+            return self.dataset
+    def _get_features(self):
+        return self.indices+self.bands
+        
+    dataset = property(_get_dataset,None,None,"""The dataset that was parsed""")
+    features = property(_get_features,None,None,"""The features that exist in this array""")
     def create_dataframe(self,subset, indices=None, bands=None):
         """Creates the dataframe ready for the algorithm processing
 
@@ -30,12 +34,12 @@ class Parser:
             [type]: [description]
         """
         if indices == None:
-            indices = INDICES+["EOPATCHES","N","P","K"]
+            indices = INDICES+["EOPATCH","N","P","K"]
         if bands == None:
             bands = BANDS
         self.indices = indices
         self.bands = bands
-        if(self.save_dataframe == None):
+        if(self.save_dataframe != None):
             return self.save_dataframe
         self.subset = subset
         all_features = indices+bands
@@ -57,17 +61,30 @@ class Parser:
         self.save_dataframe = save_dataframe
         return self.save_dataframe
         
-    def create_array(self, variable_to_fit):
+    def create_array(self, variable_to_fit,image_ids=None,features=None):
         """Creates an array ready for fitting (x,y)
 
         Args:
             variable_to_fit (string): Which variable from create_dataframe it is supposed to use for the y values
-
+            image_ids (list of int): The images in which it is supposed to include in the values, 
+                if None given, it will return all the images given in the dataframe
+            features (list of string): Name of the columns in which to include in the values,
+                if None given, it will return all the image features included in the dataframe
         Raises:
-            ExecError: [description]
+            ExecError: If the create_dataframe method wasn't called before.
         """
+
         if(self.save_dataframe == None):
             raise ExecError("Parser.create_dataframe must be called before")
-        x_values = self.save_dataframe.round(2)[self.indices+self.bands].values.reshape(-1,len(self.indices+self.bands))
-        y_values = self.save_dataframe.round(2)[variable_to_fit].values.reshape(-1,1)        
+        if(image_ids == None):
+            vals = self.save_dataframe.round(2)            
+        else:
+            vals = self.save_dataframe.round(2)[self.save_dataframe["EOPATCH"].isin(image_ids)]
+        if(features == None):
+            x_values = vals[self.indices+self.bands].values.reshape(-1,len(self.indices+self.bands))
+        else:
+            x_values = vals[features].values.reshape(-1,len(features))
+        y_values = vals[variable_to_fit].values.reshape(-1,1).ravel()        
         return (x_values,y_values)
+    
+    
