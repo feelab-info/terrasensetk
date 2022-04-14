0e0ce83d082703dc71df4639126efc0a19093ba0
Manel Pereira <afonso.pereira4525@gmail.com>
2022-04-15 00:10:06 +0100

    Fixed issues on Results, added str and repr functionn

--- utils/Results.py
+++ utils/Results.py
@@ -2,28 +2,33 @@
 class Results:
 
     def __init__(self,x_test,y_test,x_train,y_train,model,features):
-        self.x_test = x_test
-        self.y_test = y_test
-        self.x_train = x_train
-        self.y_train = y_train
-        self.model = model
-        self.features = features
+        self._x_test = x_test
+        self._y_test = y_test
+        self._x_train = x_train
+        self._y_train = y_train
+        self._model = model
+        self._features = features
     
     def _get_x_test(self):
-        return self.x_test
+        return self._x_test
     def _get_y_test(self):
-        return self.y_test
+        return self._y_test
     def _get_x_train(self):
-        return self.x_train
+        return self._x_train
     def _get_y_train(self):
-        return self.y_train
+        return self._y_train
     def _get_model(self):
-        return self.model
+        return self._model
     def _get_features(self):
-        return self.features
+        return self._features
     def _get_y_pred(self):
         return self.model.predict(self.x_test)
         
+    def __str__(self):
+        return f"x_test: {type(self._get_x_test())}\ny_test: {type(self._get_y_test())}\nx_train: {type(self._get_x_train())}\ny_train: {type(self._get_y_train())}\nmodel: {self._get_model()}\nfeatures: {self._get_features()}\ny_pred: {type(self._get_y_pred())}"
+
+    def __repr__(self):
+        return self.__str__()
     x_test = property(_get_x_test,None,None,"Gets the x_test")
     y_test = property(_get_y_test,None,None,"Gets the y_test aka groundtruth")
     x_train = property(_get_x_train,None,None,"Gets the x_train")
