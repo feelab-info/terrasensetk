8ad1ab3352eb6b6664465c4f342544a6def1abbe
Manel Pereira <afonso.pereira4525@gmail.com>
2022-05-10 01:00:23 +0100

    Adds docstring to IParser

--- dataset/parser/IParser.py
+++ dataset/parser/IParser.py
@@ -4,6 +4,17 @@ class IParser(abc.ABC):
     
     @abc.abstractmethod
     def convert(self, variable_to_fit,image_ids=None,features=None):
+        """Creates an array ready for fitting (x,y)
+
+        Args:
+            variable_to_fit (string): Which variable from create_dataframe it is supposed to use for the y values
+            image_ids (list of int): The images in which it is supposed to include in the values, 
+                if None given, it will return all the images given in the dataframe
+            features (list of string): Name of the columns in which to include in the values,
+                if None given, it will return all the image features included in the dataframe
+        Raises:
+            ExecError: If the create_dataframe method wasn't called before.
+        """
         raise NotImplementedError
 
     def create_dataframe():
