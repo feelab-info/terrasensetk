0a08c444e7c47c761ef0083ca0d3847a14d21ba4
Manel Pereira <afonso.pereira4525@gmail.com>
2022-06-27 21:21:21 +0100

    Fixes gradient boosting set_params method

--- algorithms/Regression/GradientBoostingRegressor.py
+++ algorithms/Regression/GradientBoostingRegressor.py
@@ -28,7 +28,7 @@ class GradientBoostingRegressor(IAlgorithm):
     def get_params(self):
         return self.model.get_params()
 
-    def set_params(self,**params):
+    def set_params(self,params):
         return self.model.set_params(**params)
         
     def objective_function(self,trial,x_train,y_train,x_test,y_test):
